6bf6d07bda02035cf5918fb1112d7820edcdd4be
Mika Turunen <mika.turunen@ymail.com>
2015-01-10 11:29:32 +0000

    Both angular-ui-router and AnalyticsDashboard module need the high-level mappings of the modules they depend on. Provided.

--- client/js/app.js
+++ client/js/app.js
@@ -1,40 +1,10 @@
 
 "use strict";
 
-//var angularBaseConfiguration = require("./config/analytics-dashboard");
-
-var angularDependencyMapping = [
-    // assumes that all the bootstrapping functions have .name and required functions for them to work with angular    
-    require("./dashboard-controllers/bootstrap"),
-    require("./dashboard/bootstrap")
-];
-
 // starting the 'module' names with upper case to distinguish them from the rest
 var Application = {
     name: "AnalyticsDashboard"
 };
-
-// start the application
-var configuration = function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise("/")
-    // we want the fully functional html5 mode (also removes the # -sign from the URL)
-    $locationProvider.html5Mode(true);
-
-    // defining the used states -- can we survive with just one state for the dashboard?
-    angularDependencyMapping.forEach(function(mapping) {
-        if (!mapping.states) {
-            console.log("Module", mapping.name, "does not provide routes for angular-ui-router");
-            return;    
-        }
-       
-        mapping.states.forEach(function(state) {
-            console.log("State:", state.name, "options:", JSON.stringify(state.options));
-            $stateProvider.state(state.name, state.options);
-        });  
-    });
-};
-
-configuration.$inject = [ "$stateProvider", "$urlRouterProvider", "$locationProvider" ];
 
 angular
     .module(Application.name, [
@@ -43,10 +13,10 @@ angular
             "ui.bootstrap", 
             "btford.socket-io",
         ].concat(
-            angularDependencyMapping.map(function(angularModule) { return angularModule.name; })
+            require("./dependency-mapping").map(function(angularModule) { return angularModule.name; })
         )
     )
-    .config(configuration);
+    .config(require("./config/analytics-dashboard"));
 
 console.log("Application bootstrapped");
 
