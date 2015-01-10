2c4cd4bbb783a927215242eeaa1195242e683250
Mika Turunen <mika.turunen@ymail.com>
2015-01-10 10:07:26 +0000

    Created the bootstrapper bases

---
+++ client/js/config/analytics-dashboard.js
@@ -0,0 +1,10 @@
+
+/** 
+ * @module AnalyticsDashboardConfiguration
+ * This module will contain all Angularjs related configuration for the AnalyticsDashboard module.
+ */
+var AnalyticsDashboardConfiguration = {
+
+}:
+
+module.exports = AnalyticsDashboardConfiguration;
