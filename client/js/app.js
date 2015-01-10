8d3446a358293302cb86e1ba2820b1d1cfbf55bf
Mika Turunen <mika.turunen@ymail.com>
2015-01-10 11:10:33 +0000

    The actual application now hooks itself up through iterative dependencies

--- client/js/app.js
+++ client/js/app.js
@@ -4,24 +4,50 @@
 //var angularBaseConfiguration = require("./config/analytics-dashboard");
 
 var angularDependencyMapping = [
-    // assumes that all the bootstrapping functions have .name and required functions for them to work with angular
-  //  require("./dashboard-controllers/bootstrap")
+    // assumes that all the bootstrapping functions have .name and required functions for them to work with angular    
+    require("./dashboard-controllers/bootstrap"),
+    require("./dashboard/bootstrap")
 ];
-
-/** 
- * Entry point for the application. Hotwires everything in place and basically makes sure the application is ready for
- * active use.
- */
-var bootstrapApplication = function() {
-    console.log("Application bootstrapped");
-};
 
 // starting the 'module' names with upper case to distinguish them from the rest
 var Application = {
-    bootstrap: bootstrapApplication
+    name: "AnalyticsDashboard"
 };
 
 // start the application
-Application.bootstrap();
+var configuration = function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise("/")
+    // we want the fully functional html5 mode (also removes the # -sign from the URL)
+    $locationProvider.html5Mode(true);
+
+    // defining the used states -- can we survive with just one state for the dashboard?
+    angularDependencyMapping.forEach(function(mapping) {
+        if (!mapping.states) {
+            console.log("Module", mapping.name, "does not provide routes for angular-ui-router");
+            return;    
+        }
+       
+        mapping.states.forEach(function(state) {
+            console.log("State:", state.name, "options:", JSON.stringify(state.options));
+            $stateProvider.state(state.name, state.options);
+        });  
+    });
+};
+
+configuration.$inject = [ "$stateProvider", "$urlRouterProvider", "$locationProvider" ];
+
+angular
+    .module(Application.name, [
+            // from bower or angular basic modules
+            "ui.router", 
+            "ui.bootstrap", 
+            "btford.socket-io",
+        ].concat(
+            angularDependencyMapping.map(function(angularModule) { return angularModule.name; })
+        )
+    )
+    .config(configuration);
+
+console.log("Application bootstrapped");
 
 module.exports = Application;
