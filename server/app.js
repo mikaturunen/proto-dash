cd8acf35b5cbfebae578cb0d8f70b5ca0ab7ad91
Mika Turunen <mika.turunen@ymail.com>
2015-01-13 17:15:39 +0000

    Added socket stub on server side

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var path = require("path");
 var express = require("express");
 var app = express();
@@ -21,6 +23,19 @@ app.get("*", function(request, response) {
     response.sendFile(indexHtml);
 });
 
-app.listen(app.get("port"), function() {
+var server = app.listen(app.get("port"), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+// TODO mode the socket initialization into its own place
+var socket = require("socket.io");
+var io = socket(server);
+var constants = require("./utilities/constants")
+
+io.on(constants.events.socket.connected, function(socket) {
+    console.log("Socket connected to server.",socket.id);
+    
+    socket.on(constants.events.socket.disconnected, function() {
+       console.log("Socket disconnected from server.", socket.id);
+    });
 });
