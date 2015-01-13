40798a59236a9520bd623de7ad62918f49742654
Mika Turunen <mika.turunen@ymail.com>
2015-01-13 20:17:05 +0000

    Base config

---
+++ server/database/database.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var mongodb = require("mongodb");
+var config = require("../config/config.json");
+
+var connectionString = "mongodb://<dbuser>:<dbpassword>@ds029911.mongolab.com:29911/heroku_app33167549";
---
+++ server/utilities/constants.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+    events: {
+        socket: {
+            getDasboard: "dash.get.dashboard",
+            connected: "connection",
+            disconnected: "disconnect"
+        }
+    }
+};
