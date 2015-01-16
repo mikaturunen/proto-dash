e13de10280858a91a150c1d74f927c8d62b73da8
Mika Turunen <mika.turunen@ymail.com>
2015-01-16 19:39:08 +0000

    Minor additions - now the components are collected per dashboard on the front and iterated over

--- client/dashboard/dashboard-controller.js
+++ client/dashboard/dashboard-controller.js
@@ -4,15 +4,37 @@ var name = "dash.controllers";
 var dependencyMap = require("../dependency-map").add(name);
 var constants = require("../../server/utilities/constants");
 
+var _ = require("lodash");
+
 var controller = function($scope, socket) {
-    $scope.dashboards = [];
+    $scope.allDashboards = [];
+    $scope.allComponents = [];
+    
+    $scope.dashboard = undefined;
     $scope.components = [];
     
-    socket.emit("dash.get.dashboard", {}, function(socket, result) {
-        console.log("JSON dashboard", JSON.stringify(result));
+    socket.emit("dash.get.dashboard", {}, function(socket, result) {    
+        $scope.allDashboards = result.dashboards;
+        $scope.allComponents = result.components; 
         
-        $scope.dashboards = result.dashboards;
-        $scope.components = result.components;
+        if ($scope.allDashboards.length > 0) {
+            // just select first from the set
+            $scope.dashboard = $scope.allDashboards[0];
+            // collect the included components into the active set
+            var component_ids = [];
+            $scope.dashboard.rows.forEach(function(row) {
+                Object.keys(row).forEach(function(key) {
+                    component_ids.push(row[key].component_id);
+                });
+            });
+            
+            // find all the components that match the id's
+            $scope.components = _.where($scope.allComponents, function(component) {
+                return component_ids.indexOf(component._id) !== -1;
+            });
+            
+            console.log("Visible components:", $scope.components);
+        }
     });
 };
 controller.$inject = [ "$scope", "socket" ];
