191210b35bcfb1e8c9693b44b04266422245f210
Mika Turunen <mika.turunen@ymail.com>
2015-01-23 18:16:16 +0000

    Added the new files to require chain in index.js

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ require("./gapi/gapi");
 require("./login/login-controller");
 require("./login/login-service");
 require("./socket/socket-service");
+require("./header/header-navigation-directive");
 require("./dashboard/dashboard-controller");
 require("./widgets/widget-analytics-directive");
 require("./widgets/widget-frame-directive");
