e6e8619dac0de14259d2513911da5c922149d619
Mika Turunen <mika.turunen@ymail.com>
2015-01-29 07:23:31 +0000

    Refactored the gapi service a bit and commented the functions

--- client/gapi/gapi.js
+++ client/gapi/gapi.js
@@ -12,13 +12,21 @@ var constants = require("../../server/utilities/constants");
 var gapiIsReadyDeferred;
 var gapiIsReadyPromise;
 
-// TODO COMMENTS!
-
+/** 
+ * Service for immediate GAPI usage; commonly one would use it through the $window service but 
+ * this service abstracts few behaviors such as the more or less broken authentication/authorization of gapi. I'm not even
+ * kidding.
+ * @param  {ng-service} $q          
+ * @param  {ng-service} $window     
+ * @param  {ng-service} $state      
+ * @param  {ng-service} gapiservice 
+ */
 var service = ($q, $window, $state, gapiservice) => {
-    var get = () => {
-        return gapiIsReadyPromise;
-    };
-
+    /**
+     * Attempts to authenticate the provided client.
+     * @param  {{ success: string; failed: string; }}} redirectOptions Options for redirecting the user if necessary. 
+     * @return {Promise}                 Promise resolving to true on success authentication.
+     */
     var auth = (redirectOptions) => {
         var deferred = $q.defer();
 
@@ -42,6 +50,11 @@ var service = ($q, $window, $state, gapiservice) => {
         return deferred.promise;
     };
 
+    /**
+     * Checks if the current client is authorized through gapi to see google's services. Even if user fakes this result
+     * all the calls to google will fail. 
+     * @return {Promise} True when authorized.
+     */
     var isAuthorized = () => {
         var deferred = $q.defer();
         
@@ -69,39 +82,53 @@ var service = ($q, $window, $state, gapiservice) => {
 service.$inject = [ "$q", "$window", "$state", "gapiservice" ];
 service.serviceName = "gapi";
 
+/** 
+ * Run block for the current module. This makes sure the gapi gets injected into the current web page in most
+ * hassle free way possible. 
+ * @param  {ng-service} $q              
+ * @param  {ng-service} gapiservice  
+ */
+var run = ($q, gapiservice) => {
+    gapiIsReadyDeferred = $q.defer();
+    gapiIsReadyPromise = gapiIsReadyDeferred.promise;
+
+    // NOTE I don't agree at all with the gay GAPI operates and how it has been designed to 'dynamically' be injected
+    //      into the website. I think it breaks too many princibles of a modern web application. This is not a good thing.
+    //      This is a quick attempt to just try and go ahead in wrapping the horrible implementation of gapi into a more
+    //      decent usage case and to make sure it would behave nicely with the Angular applications life cycle.
+
+    // Wow, I'm surprised there's no really clean way of doing this.. I hate myself for this
+    var scriptText = "" +
+       "(function(w,d,s,g,js,fs){ " +
+            "g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}}; " +
+            "js=d.createElement(s);fs=d.getElementsByTagName(s)[0]; " +
+            "js.src='https://apis.google.com/js/platform.js'; " +
+            "fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');}; " +
+       "}(window,document,'script'));  ";
+    
+    var gaCode = document.createTextNode(scriptText);
+    var scriptTag = document.createElement("script");
+    scriptTag.type = "text/javascript";
+    
+    // slam them dynamically into the body and be done with it. My soul weeps.
+    scriptTag.appendChild(gaCode);
+    document.body.appendChild(scriptTag);
+
+    // now somewhat normalized behavior, quickly wrap the beast into promise 
+    gapi.analytics.ready(() => {
+        gapiservice.loadApis().then(() => {
+            console.log("gapi.analytics is ready for use.");
+            gapiIsReadyDeferred.resolve(gapi);
+        }, 
+        () => {
+            console.log("Failed to load required apis for gapi.");
+            gapiIsReadyDeferred.reject(false); 
+        });
+    });
+};
+run.$inject = [ "$q", "gapiservice" ];
+
 angular
     .module(name)
-    .run(["$q", "gapiservice", ($q, gapiservice) => {
-        gapiIsReadyDeferred = $q.defer();
-        gapiIsReadyPromise = gapiIsReadyDeferred.promise;
-
-        // Wow, I'm surprised there's no really clean way of doing this.. I hate myself for this
-        var scriptText = "" +
-           "(function(w,d,s,g,js,fs){ " +
-                "g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}}; " +
-                "js=d.createElement(s);fs=d.getElementsByTagName(s)[0]; " +
-                "js.src='https://apis.google.com/js/platform.js'; " +
-                "fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');}; " +
-           "}(window,document,'script'));  ";
-        
-        var gaCode = document.createTextNode(scriptText);
-        var scriptTag = document.createElement("script");
-        scriptTag.type = "text/javascript";
-        
-        // slam them dynamically into the body and be done with it. My soul weeps.
-        scriptTag.appendChild(gaCode);
-        document.body.appendChild(scriptTag);
-
-        // now somewhat normalized behavior, quickly wrap the beast into promise 
-        gapi.analytics.ready(() => {
-            gapiservice.loadApis().then(() => {
-                console.log("gapi.analytics is ready for use.");
-                gapiIsReadyDeferred.resolve(gapi);
-            }, 
-            () => {
-                console.log("Failed to load required apis for gapi.");
-                gapiIsReadyDeferred.reject(false); 
-            });
-        });
-    }])
+    .run(run)
     .service(service.serviceName, service);
