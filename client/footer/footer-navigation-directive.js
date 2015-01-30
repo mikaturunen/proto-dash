eb78a99619415a49bbddb9e5cfb29ffd9acd13bc
Mika Turunen <mika.turunen@ymail.com>
2015-01-30 17:36:46 +0000

    simple footer in place

---
+++ client/footer/footer-navigation-directive.js
@@ -0,0 +1,28 @@
+/* global gapi, console, require, angular, document */
+/* jslint node: true */
+/* jshint esnext: true */
+
+"use strict";
+
+var name = "dash.footer";
+var dependencyMap = require("../dependency-map").add(name);
+
+var link = (scope, element, attributes) => {
+};
+
+var directive = () => {
+    return {
+        restrict: "E",
+        scope: {
+            dashboard: "="
+        },
+        templateUrl: "/public/html/footer/footer-navigation-template.html",
+        link: link
+    };
+};
+directive.directiveName = "footerNavigation";
+
+angular
+    .module(name)
+    .directive(directive.directiveName, directive);
+
