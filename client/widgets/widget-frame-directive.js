c1c7db74b77f669c3051c4c5507cd42ffbc7840c
Mika Turunen <mika.turunen@ymail.com>
2015-01-31 10:03:10 +0000

    Wrote a simple directive for hooking into html eleemnts onload events - used with iframes dynamic resizing

--- client/widgets/widget-frame-directive.js
+++ client/widgets/widget-frame-directive.js
@@ -8,37 +8,73 @@ var name = "dash.widgets";
 var dependencyMap = require("../dependency-map").add(name);
 var constants = require("../../server/utilities/constants");
 
-var link = ($sce) => {
-    return (scope, element, attributes) => {
+/** 
+ * Angular directive for Iframe based widgets to show external content on the Dashboard.
+ * @param  {ng-service} $sce
+ */
+var elementDirective = ($sce) => {
+    /**
+     * Linker function for the direcvite.
+     * @param  {ng-scope} scope     
+     * @param  {HtmlElement} element    
+     * @param  {Object} attributes 
+     */
+    var elementDirectiveLink = (scope, element, attributes) => {
         if (!(scope.isActive = scope.component.type === "IFRAME")) {
             return;
         }
 
-        if (scope.component.source.indexOf("docs.google.com") !== -1 && 
-            scope.component.source.slice(-13) !== "&output=embed") {
-            // TODO fix the if statement and apply for a proper embed option
-        }
-        
-        var url = (scope.component.source + "&embedded=true");
-      
-        scope.component.source = $sce.trustAsResourceUrl(url);
-    };
-};
+        scope.dynamicResize = () => {
+            console.log("Resizing the iframe to fill container..", element);
+        };
 
-var directive = ($sce) => {
+        scope.component.source = $sce.trustAsResourceUrl(scope.component.source);
+    };
+
     return {
         restrict: "E",
         scope: {
             component: "="
         },
         templateUrl: "/public/html/widgets/widget-frame-template.html",
-        link: link($sce)
+        link: elementDirectiveLink
     };
 };
-directive.$inject = [ "$sce" ];
-directive.directiveName = "widgetFrame";
+elementDirective.$inject = [ "$sce" ];
+elementDirective.directiveName = "widgetFrame";
+
+// There is no direct way of binding angular to elements onload event (as javascript elements onload callback looks into
+// window.callback (global function scope)) and angular is extremely finicky with that and it does not work with the 
+// common way of thinking with angular. To avoid the issue we create simple angular directive that just takes care 
+// of the onload ballback of the iframe element and this way we get nicely wrapped onload functionality in angular :)
+
+/**
+ * Creates a angular scope bound callback to allow easier angular style callbacks into specific elements onload callback
+ * through the custom 'element-onload' attribute.
+ * 
+ * Solves case:
+ *     <iframe src="www.google.com" onload="callbackGlobalFunction()"></iframe>
+ *     
+ * Example usage:
+ *     <iframe src="www.google.com" element-onload="angularScopeCallback()"></iframe>
+ */
+var elementOnloadDirective = () => {
+    return {
+        restrict: "A",
+        scope: {
+            callback: "&elementOnload"
+        },
+        link: (scope, element, attrs) => {
+            // hooking up the onload event
+            element.on("load", () => scope.callback());
+        }
+    };
+};
+elementOnloadDirective.$inject = [ ];
+elementOnloadDirective.directiveName = "elementOnload";
 
 angular
     .module(name)
-    .directive(directive.directiveName, directive);
+    .directive(elementOnloadDirective.directiveName, elementOnloadDirective)
+    .directive(elementDirective.directiveName, elementDirective);
 
