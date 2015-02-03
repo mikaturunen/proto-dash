0575397dfb9443af24e408a75697ffa3c9dd9bb7
Mika Turunen <mika.turunen@ymail.com>
2015-02-03 17:35:38 +0000

    First extremely rude placement for the calendar popups.

--- client/dependency-map.js
+++ client/dependency-map.js
@@ -4,12 +4,20 @@
 
 "use strict";
 
+// Dynamically loads all the specified dependencies into Angulars context and allows easier injection into the scope
+// from a centralized location
+
 var dependencies = [ ];
 module.exports = {
+    /** 
+     * {Array<string>} List of dependencies for Angular to include into the main module of the application.
+     */
     dependencies: dependencies,
     
     /** 
      * Adds a new dependency to Angular
+     * @param {string} dependency Name of the dependency
+     * @param {Array<string>} dependsOn (Optional) List of dependencies the angular module might depend on. 
      */
     add: function(dependency, dependsOn) {
         if (dependencies.indexOf(dependency) !== -1) {
--- client/widgets/widget-analytics-directive.js
+++ client/widgets/widget-analytics-directive.js
@@ -13,6 +13,41 @@ var directive = (gapiservice) => {
         if (!(scope.isActive = scope.component.type === "GOOGLE_ANALYTICS")) {
             return;
         }
+    
+        /** 
+         * Generalized open popup method that can accept different scope variables to
+         * be modified on opening of the function.
+         * @param {Object} scope Directive scope.
+         * @param {string} variable String name of the property to modify in the scope.
+         * @returns {Function} Callback for the open event.
+         */
+        var open = (scope, variable) => {
+              scope[variable] = false;
+            
+              /**
+               * Opens the popup for the datepicker.
+               * @param {Object} $event Angular wrapped event object.
+               */
+              return ($event) => {
+                  console.log("Opening popup..");
+                  $event.preventDefault();
+                  $event.stopPropagation();
+                  
+                  scope[variable] = true;
+              }; 
+        };
+        
+        //- TODO Read the values from database
+        //- TODO Save the values into the database when user selects something
+        scope.startDate = new Date();
+        scope.endDate = new Date();
+       
+        scope.dateOptions = {
+           formatYear: 'yy',
+           startingDay: 1
+        };
+        scope.openStart = open(scope, "startOpened");
+        scope.openEnd = open(scope, "endOpened");
 
         gapiservice.generate(scope.component.gapi);
     };
