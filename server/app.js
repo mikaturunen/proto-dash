fc432ca2cf68cf69d453baa6f22921fda16d5368
Mika Turunen <mika.turunen@ymail.com>
2015-02-14 08:14:48 +0000

    Custom port variable for nitrous.io side-by-side develomepment and removed transformer function.

--- server/app.js
+++ server/app.js
@@ -27,7 +27,10 @@ var initRoutes = () => {
         { directory: path.join(releaseDirectory, "../node_modules/socket.io-client/socket.io.js"), url: "/socket.io.js" }
     ];
 
-    app.set("port", (process.env.PORT || 3000));
+    // This custom variable overwrites all other port variables
+    var port = process.env.DEV_PORT ? process.env.DEV_PORT : (process.env.PORT || 3000);
+    app.set("port", port);
+    
     staticRouteMappings.forEach(mapping => {
         app.use(mapping.url, express.static(mapping.directory));                      
     });
@@ -55,35 +58,6 @@ var initServer = () => {
     });
     
     return deferred.promise;
-};
-
-// TODO move the transformers into their own module. they are taking too much space and bring in unnecessary glutter here
-var _ = require("lodash");
-var mongo = require("mongodb");
-var ObjectID = mongo.ObjectID;
-/** 
- * Transformers the array of resulting component documents into the rows x columns format specified in the dashboard
- * that rows_component_ids represent with pure englighted magic and unicorns.
- * @param {string[][]} rows_component_ids 2d array with the first layer being rows and the second dimension being columns
- * @returns Components transformed into the replica of rows_component_ids but instead of id's, the actual objects
- */
-var arrangeComponentsIntoRowsTransformer = (rows_component_ids) => {
-    return (results) => {
-        // don't ask, I'm just throwing some map magic at it. Basically what is being done is I want the actual objects
-        // to represent the rows in the exact same format as rows_component_ids are. So rows_component_ids describe
-        // the format for us and we just fill in the details with the map magic and store the component objects in the 
-        // exactly same format as the rows_component_ids is: Array<Array<Object>>, number or rows and colums is dynamic
-        
-        return rows_component_ids.map(rowColumns => {
-            return rowColumns.map(component_id => { 
-                // we make sure that we never get undefined into the collection. Makes our life a little easier on front
-                var component = _.find(results, componentDocument => {
-                    return componentDocument._id.equals(component_id);
-                }) || { }; 
-               return component;
-            });
-        });
-    };
 };
 
 /** 
